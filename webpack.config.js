f68868885cc2b913377cb04f3f457ecc591eb40d
tgtiburon <tg.tiburon@gmail.com>
2022-02-08 18:59:18 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ module.exports = {
                             },
                             publicPath: function(url) {
                                 return url.replace("../", "/assets/")
+                                
                             }
 
                         }
